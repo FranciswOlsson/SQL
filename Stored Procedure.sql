ef0bec7172c08149a9525e269a8715b45eaf83e7
FranciswOlsson <39450374+FranciswOlsson@users.noreply.github.com>
2018-11-15 19:12:44 -0800

    Add files via upload

---
+++ Stored Procedure.sql
@@ -0,0 +1,29 @@
+/*Task 1: Create Stored Procedure*/
+
+CREATE Database Members #Create Members database
+
+CREATE Procedure GetAllMembers  #Create new procedure
+AS
+SELECT * TableMembers  #Select all members from table of members
+
+CREATE Procedure [Members].[UpdateMember] #Modify Firstname
+@Firstname varchar(50)  #Set Firstname to character with length of 50
+AS
+UPDATE TableMembers  
+SET Firstname = @Firstname #Redeclare Firstname via user defined format
+WHERE Firstname = 'Frank'
+
+
+
+
+
+
+
+
+
+/*
+
+
+
+
+
